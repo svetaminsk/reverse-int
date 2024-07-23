d3876d02fb0e02f1e9faf3f77bf4cf5f095ddb78
Svetlana Evdokimova <7757331@gmail.com>
2024-07-23 16:26:28 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const s = String(n);
+    let result = '';
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== '-') {
+            result += s[i];
+        }
+    } return Number(result);
 }
